2b7e861d7f444008feb5ea84ada5abaf462b6d4d
changxianglin <281532612@qq.com>
2023-08-11 02:21:56 +0800

    element-plus internation and ts-ignore

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
+    <el-button type="success" size="default" :icon="Edit">编辑按钮</el-button>
+    <el-button type="danger" size="default" :icon="Delete">删除按钮</el-button>
+
+    <el-pagination
+      :page-sizes="[100, 200, 300, 400]"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-  import { Plus } from '@element-plus/icons-vue'
+  import { Plus, Edit, Delete } from '@element-plus/icons-vue'
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@
 // main.ts
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
@@ -16,6 +18,8 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
 
