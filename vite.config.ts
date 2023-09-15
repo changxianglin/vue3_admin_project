b0f3548b92fdacecc9e0cfb2c64f9357f37bec24
changxianglin <281532612@qq.com>
2023-09-15 23:54:21 +0800

    use vite-plugin-mock should to version 2.9.8

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,33 @@ import path from 'path'
 
 // import svg need plugin
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(),
-  createSvgIconsPlugin({
-    iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-    symbolId: 'icon-[dir]-[name]',
-  })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src')
-    }
-  },
-  // scss global
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: '@import "./src/styles/variable.scss";'
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      viteMockServe({
+        localEnabled: command === 'serve',
+      }),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    },
+    // scss global
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: '@import "./src/styles/variable.scss";'
+        }
       }
     }
   }
