9571dd38ec43c8f36decd57928b52a9979309d85
changxianglin <281532612@qq.com>
2023-09-23 00:00:14 +0800

    error code

--- utils/request.ts
+++ utils/request.ts
@@ -6,6 +6,31 @@ let request = axios.create({
   timeout: 5000
 })
 
-request.interceptors.request.use(config => {
+request.interceptors.request.use((config) => {
   return config
+})
+
+
+request.interceptors.response.use((response) => {
+    return response.data
+}, (error) => {
+  let message = ''
+  let status = error.response.status
+  switch (status) {
+    case 401:
+      message = 'TOKEN过期'
+      break;
+    case 403:
+      message = '无权访问'
+      break;
+    case 404:
+      message = '地址错误'
+      break;
+    case 500:
+      message = '服务器出现问题'
+      break;
+    default:
+      message = '网络出现问题'
+      break;
+  }
 })
