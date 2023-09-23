8783fbe3999a5ffae64b4af4f605dd3655acc327
changxianglin <281532612@qq.com>
2023-09-23 23:59:45 +0800

    use element plus

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="login">login</div>
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0">占位文字</el-col>
+      <el-col :span="12" :xs="24">右侧表单</el-col>
+    </el-row>
+  </div>
 </template>
 
 <script lang='ts' setup>
 
 </script>
 
-<style lang='less' scoped>
-
+<style lang='scss' scoped>
+  .login_container {
+    width: 100%;
+    height: 100vh;
+  }
 </style>
