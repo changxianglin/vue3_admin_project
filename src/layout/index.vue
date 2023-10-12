7d13701175c2a99cc0b00a87aa3f96e18341ebe8
changxianglin <281532612@qq.com>
2023-10-12 23:57:37 +0800

    layout menus scrollbar

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,6 +2,10 @@
   <div class="layout_container">
     <div class="layout_slider">
       <Logo />
+      <!-- menu -->
+      <el-scrollbar class="scrollbar">
+        <p v-for="item in 2000" :key="item" class="scrollbar-demo-item">{{item}}</p>
+      </el-scrollbar>
     </div>
     <div class="layout_tabbar"></div>
     <div class="layout_main"></div>
@@ -16,10 +20,16 @@ import Logo from './logo/index.vue'
 .layout_container {
   width: 100%;
   height: 100vh;
+  color: #fff;
   .layout_slider {
     width: $base-menu-width;
     height: 100vh;
     background: $base-menu-background;
+
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
+    }
   }
   .layout_tabbar {
     position: fixed;
