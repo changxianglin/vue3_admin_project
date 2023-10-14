e4e0d758954a3d27d4f9854b3f15cb06c56453d5
changxianglin <281532612@qq.com>
2023-10-14 10:10:54 +0800

    finish menu icon

--- src/layout/menu/index.vue
+++ src/layout/menu/index.vue
@@ -2,9 +2,11 @@
   <template v-for="(item, index) in menuList" :key="item.path">
     <!-- 无子路由 -->
     <template v-if="!item.children">
-      <el-menu-item :index="item.path" v-if="!item.meta.hidden">
+      <el-menu-item :index="item.path" v-if="!item.meta.hidden" @click="goRoute">
         <template #title>
-          <span>icon&nbsp</span>
+          <el-icon>
+            <component :is="item.meta.icon"></component>
+          </el-icon>
           <span>{{ item.meta.title }}</span>
         </template>
       </el-menu-item>
@@ -13,6 +15,9 @@
     <template v-if="item.children && item.children.length == 1">
       <el-menu-item :index="item.children[0].path" v-if="!item.children[0].meta.hidden">
         <template #title>
+          <el-icon>
+            <component :is="item.children[0].meta.icon"></component>
+          </el-icon>
           <span>{{ item.children[0].meta.title }}</span>
         </template>
       </el-menu-item>
@@ -20,6 +25,9 @@
     <!-- 多个子路由 -->
     <el-sub-menu :index="item.path" v-if="item.children && item.children.length > 1">
       <template #title>
+        <el-icon>
+          <component :is="item.meta.icon"></component>
+        </el-icon>
         <span>{{ item.meta.title }}</span>
       </template>
       <Menu :menu-list="item.children" ></Menu>
@@ -29,6 +37,10 @@
 
 <script lang='ts' setup>
 defineProps(['menuList'])
+
+const goRoute = (vc) => {
+  console.log(vc)
+}
 </script>
 
 <script lang="ts">
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,6 +6,7 @@ export const constantRoute = [
     meta: {
       title: '登录',
       hidden: true,
+      icon: 'Promotion',
     },
   },
   {
@@ -15,6 +16,7 @@ export const constantRoute = [
     meta: {
       title: 'layout',
       hidden: false,
+      icon: 'Avatar',
     },
     children: [
       {
@@ -23,16 +25,9 @@ export const constantRoute = [
         meta: {
           title: '首页',
           hidden: false,
+          icon: 'HomeFilled',
         },
       },
-      {
-        path: '/ceshi',
-        component: () => import('@/views/home/index.vue'),
-        meta: {
-          title: '测试',
-          hidden: false,
-        },
-      }
     ]
   }, 
   {
@@ -42,6 +37,7 @@ export const constantRoute = [
     meta: {
       title: '404',
       hidden: true,
+      icon: 'Cellphone',
     },
   },
   {
@@ -51,6 +47,7 @@ export const constantRoute = [
     meta: {
       title: '任意路由',
       hidden: true,
+      icon: 'Monitor',
     },
   }
 ]
