62d03ed378ba52abbfd876a9ec74a8300a7ab0a3
changxianglin <281532612@qq.com>
2023-10-14 23:58:50 +0800

    add tabbar

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -9,7 +9,9 @@
         </el-menu>
       </el-scrollbar>
     </div>
-    <div class="layout_tabbar"></div>
+    <div class="layout_tabbar">
+      <Tabbar />
+    </div>
     <div class="layout_main">
       <Main></Main>
     </div>
@@ -21,6 +23,7 @@ import { useRoute } from 'vue-router'
 import Logo from './logo/index.vue'
 import Menu from './menu/index.vue'
 import Main from './main/index.vue'
+import Tabbar from './tabbar/index.vue'
 
 import useUserStore from '@/store/modules/user'
 
@@ -32,11 +35,11 @@ const route = useRoute()
 .layout_container {
   width: 100%;
   height: 100vh;
-  color: #fff;
   .layout_slider {
     width: $base-menu-width;
     height: 100vh;
     background: $base-menu-background;
+    color: #fff;
 
     .scrollbar {
       width: 100%;
@@ -52,7 +55,6 @@ const route = useRoute()
     left: $base-menu-width;
     width: calc(100% - $base-menu-width);
     height: $base-tabbar-height;
-    background: cyan;
   }
   .layout_main {
     position: absolute;
--- src/layout/tabbar/index.vue
+++ src/layout/tabbar/index.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>tbbar</div>
+  <div class="tabbar">
+    <div class="tabbar_left">
+      <el-icon style="margin-right: 10px;">
+        <Expand />
+      </el-icon>
+      <!--  -->
+      <el-breadcrumb separator-icon="ArrowRight">
+        <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="tabbar_right">
+      <el-button type="primary" size="small" icon="Refresh" circle></el-button>
+    </div>
+  </div>
 </template>
 
 <script lang='ts' setup>
 
 </script>
 
-<style lang='less' scoped>
+<style lang='scss' scoped>
+.tabbar {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
 
+  .tabbar_left {
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+  }
+}
 </style>
