c688664ea9d62bd0e0baf98f2c4e2217008adcad
changxianglin <281532612@qq.com>
2023-11-05 10:45:26 +0800

    router auth

--- src/permission.ts
+++ src/permission.ts
@@ -1,10 +1,33 @@
 import router from "@/router";
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css'
+import useUserStore from "./store/modules/user";
+import pinia from "./store";
+const userStore = useUserStore(pinia)
+
+console.log(userStore)
 
 router.beforeEach((to: any, from: any, next: any) => {
   NProgress.start()
-  next()
+  const token = userStore.token
+  const userName = userStore.username
+  if(token) {
+    if(to.path == '/login') {
+      next({path: '/'})
+    } else {
+      if(userName) {
+        next()
+      } else {
+        
+      }
+    }
+  } else {
+    if(to.path == '/login') {
+      next()
+    } else {
+      next({path: '/login', query: {redirect: to.path}})
+    }
+  }
 })
 
 
