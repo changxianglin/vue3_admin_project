65cf6a18ad0623f095287cffc0628733c4d79d25
changxianglin <281532612@qq.com>
2023-11-11 11:12:48 +0800

    add pagination

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -9,13 +9,24 @@
       <el-table-column label="品牌LOGO"></el-table-column>
       <el-table-column label="品牌操作"></el-table-column>
     </el-table>
+    <!-- pagination -->
+    <el-pagination 
+      v-model:current-page="pageNo" 
+      v-model:page-size="limit" 
+      :page-sizes="[3, 5, 7, 9]"
+        :background="true" 
+        layout="prev, pager, next, jumper, ->, sizes, total,"
+        :total="400" 
+        @size-change="handleSizeChange" 
+        @current-change="handleCurrentChange" 
+      />
   </el-card>
 </template>
 
 <script lang='ts' setup>
-
+import { ref } from 'vue'
+const pageNo = ref<number>(1)
+const limit = ref<number>(3)
 </script>
 
-<style lang='less' scoped>
-
-</style>
+<style lang='less' scoped></style>
