bfc261bf6d9cb0e7ea863d5fd1c77281be6af078
changxianglin <281532612@qq.com>
2023-11-11 16:01:15 +0800

    add trademark type

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,7 +1,8 @@
 import request from '@/utils/request'
+import type { TradeMarkResponseData } from './type'
 
 enum API {
   TRADEMARK_URL = "/admin/product/baseTrademark/"
 }
 
-export const reqHasTrademark = (page: number, limit: number) => request.get<any, any>(API.TRADEMARK_URL + `${page}/${limit}`)
+export const reqHasTrademark = (page: number, limit: number) => request.get<any, TradeMarkResponseData>(API.TRADEMARK_URL + `${page}/${limit}`)
