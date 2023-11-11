245aff640419b8ef9fb8bf376b7419096ab8304c
changxianglin <281532612@qq.com>
2023-11-11 11:55:58 +0800

    request trademark and render table

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -3,9 +3,9 @@
     <!-- button -->
     <el-button type="primary" size="default" icon="Plus" @click="">添加品牌</el-button>
     <!-- table -->
-    <el-table style="margin: 10px 0px;" border>
-      <el-table-column label="序号" width="80px" align="center"></el-table-column>
-      <el-table-column label="品牌名称"></el-table-column>
+    <el-table style="margin: 10px 0px;" border :data="trademark">
+      <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
+      <el-table-column label="品牌名称" prop="tmName"></el-table-column>
       <el-table-column label="品牌LOGO"></el-table-column>
       <el-table-column label="品牌操作"></el-table-column>
     </el-table>
@@ -16,7 +16,7 @@
       :page-sizes="[3, 5, 7, 9]"
         :background="true" 
         layout="prev, pager, next, jumper, ->, sizes, total,"
-        :total="400" 
+        :total="total" 
         @size-change="handleSizeChange" 
         @current-change="handleCurrentChange" 
       />
@@ -24,9 +24,24 @@
 </template>
 
 <script lang='ts' setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import { reqHasTrademark } from '@/api/product/trademark'
 const pageNo = ref<number>(1)
 const limit = ref<number>(3)
+const total = ref<number>(0)
+const trademark = ref<any>([])
+
+onMounted(() => {
+  getHasTrademark()
+})
+
+const getHasTrademark = async () => {
+  const result = await reqHasTrademark(pageNo.value, limit.value)
+  if(result.code == 200) {
+    total.value = result.data.total
+    trademark.value = result.data.records
+  }
+}
 </script>
 
 <style lang='less' scoped></style>
