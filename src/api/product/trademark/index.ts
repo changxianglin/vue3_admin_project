8047545ae49d4e4af053aaedcfa9feb601d8a130
changxianglin <281532612@qq.com>
2023-11-19 10:59:22 +0800

    add request api

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,8 +1,20 @@
 import request from '@/utils/request'
-import type { TradeMarkResponseData } from './type'
+import type { TradeMarkResponseData, TradeMark } from './type'
 
 enum API {
-  TRADEMARK_URL = "/admin/product/baseTrademark/"
+  TRADEMARK_URL = "/admin/product/baseTrademark/",
+  ADDTRADEMARK_URL = "/admin/product/baseTrademark/save",
+  UPDATETRADEMARK_URL = "/admin/product/baseTrademark/update",
 }
 
 export const reqHasTrademark = (page: number, limit: number) => request.get<any, TradeMarkResponseData>(API.TRADEMARK_URL + `${page}/${limit}`)
+
+// add or update
+export const reqAddOrUpdateTrademark = (data: TradeMark) => {
+  // update
+  if(data.id) {
+    return request.put(API.UPDATETRADEMARK_URL, data)
+  } else { // update
+    return request.post(API.ADDTRADEMARK_URL, data)
+  }
+}
