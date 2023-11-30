73076a99abedde81f8d77616d51c62dffe222f10
changxianglin <281532612@qq.com>
2023-11-30 23:06:06 +0800

    write attr static page

---
+++ src/components/Category/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-card>
+      <el-form :inline="true">
+        <el-form-item label="一级分类">
+          <el-select>
+            <el-option label="北京"></el-option>
+            <el-option label="上海"></el-option>
+            <el-option label="广州"></el-option>
+            <el-option label="深圳"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="二级分类">
+          <el-select>
+            <el-option label="北京"></el-option>
+            <el-option label="上海"></el-option>
+            <el-option label="广州"></el-option>
+            <el-option label="深圳"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="三级分类">
+          <el-select>
+            <el-option label="北京"></el-option>
+            <el-option label="上海"></el-option>
+            <el-option label="广州"></el-option>
+            <el-option label="深圳"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script lang='ts' setup>
+
+</script>
+
+<style lang='less' scoped>
+
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import SvgIcon from './SvgIcon/index.vue'
 import Pagination from './Pageination/index.vue'
+import Category from './Category/index.vue'
 
 const allGlobalComponent = {
   SvgIcon,
   Pagination,
+  Category,
 }
 
 export default {
