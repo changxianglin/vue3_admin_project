69cb46ef580e4f7757843b2db62f1c553a07f087
changxianglin <281532612@qq.com>
2024-06-16 11:08:15 +0800

    add and change product attr

--- src/components/Category/index.vue
+++ src/components/Category/index.vue
@@ -3,17 +3,17 @@
     <el-card>
       <el-form :inline="true">
         <el-form-item label="一级分类">
-          <el-select v-model="categoryStore.c1Id" @change="handler">
+          <el-select :disabled="scene == 0 ? false : true" v-model="categoryStore.c1Id" @change="handler">
             <el-option :label="c1.name" v-for="(c1, index) in categoryStore.c1Arr" :key="c1.id" :value="c1.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="二级分类">
-          <el-select v-model="categoryStore.c2Id" @change="handler1">
+          <el-select :disabled="scene == 0 ? false : true" v-model="categoryStore.c2Id" @change="handler1">
             <el-option v-for="(c2, index) in categoryStore.c2Arr" :key="c2.id" :label="c2.name" :value="c2.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="三级分类">
-          <el-select v-model="categoryStore.c3Id">
+          <el-select :disabled="scene == 0 ? false : true" v-model="categoryStore.c3Id">
             <el-option :label="c3.name" v-for="(c3, index) in categoryStore.c3Arr" :key="c3.id" :value="c3.id"></el-option>
           </el-select>
         </el-form-item>
@@ -47,6 +47,8 @@ const handler1 = () => {
   categoryStore.c3Id = '' 
   categoryStore.getC3()
 }
+
+defineProps(['scene'])
 </script>
 
 <style lang='less' scoped>
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,22 +1,40 @@
 <template>
-    <Category />
+    <Category :scene="scene" />
     <el-card style="margin: 10px 0;">
-      <el-button type="primary" size="default" icon="Plus" :disabled="!categoryStore.c3Id">添加属性</el-button>
-      <el-table border style="margin: 10px 0;" :data="attrArr">
-        <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
-        <el-table-column label="属性名称" width="120px" prop="attrName"></el-table-column>
-        <el-table-column label="属性值名称">
-          <template #="{row, index}">
-            <el-tag style="margin: 5px;" v-for="(item, index) in row.attrValueList" :key="item.id">{{ item.valueName }}</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column label="操作" width="120px">
-          <template #="{row, index}">
-            <el-button type="primary" size="small" icon="Edit"></el-button>
-            <el-button type="primary" size="small" icon="Delete"></el-button>
-          </template>
-        </el-table-column>
+      <div v-show="scene == 0">
+        <el-button type="primary" size="default" icon="Plus" :disabled="!categoryStore.c3Id" @click="addAttr">添加属性</el-button>
+        <el-table border style="margin: 10px 0;" :data="attrArr">
+          <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
+          <el-table-column label="属性名称" width="120px" prop="attrName"></el-table-column>
+          <el-table-column label="属性值名称">
+            <template #="{row, index}">
+              <el-tag style="margin: 5px;" v-for="(item, index) in row.attrValueList" :key="item.id">{{ item.valueName }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" width="120px">
+            <template #="{row, index}">
+              <el-button type="primary" size="small" icon="Edit" @click="updateAttr"></el-button>
+              <el-button type="primary" size="small" icon="Delete"></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+    </div>
+    <div v-show="scene == 1">
+      <el-form :inline="true">
+        <el-form-item label="属性名称">
+          <el-input palceholder="请输入属性名称"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button icon='Plus' type="primary">添加属性</el-button>
+      <el-button @click="cancel">取消</el-button>
+      <el-table border style="margin: 10px 0px;">
+        <el-table-column label="序号" width="80px" type="index" align="center"></el-table-column>
+        <el-table-column label="属性值"></el-table-column>
+        <el-table-column label="属性值操作"></el-table-column>
       </el-table>
+      <el-button type="primary">保存</el-button>
+      <el-button @click="cancel">取消</el-button>
+    </div>
     </el-card>
 </template>
 
@@ -29,6 +47,7 @@ import useCategoryStore from '@/store/modules/category';
 const categoryStore = useCategoryStore()
 
 const attrArr = ref<Attr[]>([])
+const scene = ref<number>(1)
 
 watch(() => categoryStore.c3Id, () => {
   attrArr.value = []
@@ -42,6 +61,18 @@ const getAttr = async () => {
   if(result.code == 200) {
     attrArr.value = result.data
   }
+}
+
+const addAttr = () => {
+  scene.value = 1
+}
+
+const updateAttr = () => {
+  scene.value = 1
+}
+
+const cancel = () => {
+  scene.value = 0
 }
 </script>
 
