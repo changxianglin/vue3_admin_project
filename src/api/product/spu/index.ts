546b758fc85a619e0b4e130ffeadbb4d6e7f6330
changxianglin <281532612@qq.com>
2024-12-23 20:36:32 +0800

    request and show spu

---
+++ src/api/product/spu/index.ts
@@ -0,0 +1,8 @@
+import request from '@/utils/request'
+import type { HasSpuResponseData } from './type'
+
+enum API {
+  HASSPU_URL = '/admin/product/'
+}
+
+export const reqHasSpu = (page: number, limit: number, category3Id: string|number) => request.get<any, HasSpuResponseData>(API.HASSPU_URL + `${page}/${limit}?category3Id=${category3Id}`) 
--- src/views/product/spu/index.vue
+++ src/views/product/spu/index.vue
@@ -3,13 +3,20 @@
     <Category :scene="scene"></Category>
 
     <el-card style="margin: 10px 0px;">
-      <el-button type="primary" icon="Plus">添加 SPU</el-button>
+      <el-button type="primary" icon="Plus" :disabled="categoryStore.c3Id ? false : true">添加 SPU</el-button>
 
-      <el-table style="margin: 10px 0;" border>
+      <el-table style="margin: 10px 0;" border :data="records">
         <el-table-column label="序号" type="index" align="center" width="80"></el-table-column>
-        <el-table-column label="SPU名称"></el-table-column>
-        <el-table-column label="SPU描述"></el-table-column>
-        <el-table-column label="SPU操作"></el-table-column>
+        <el-table-column label="SPU名称" prop="spuName"></el-table-column>
+        <el-table-column label="SPU描述" prop="description" show-overflow-tooltip></el-table-column>
+        <el-table-column label="SPU操作">
+          <template #default="scope">
+            <el-button  size="small" icon="Plus" title="添加SKU"></el-button>
+            <el-button type="info" size="small" icon="Edit" title="修改SKU"></el-button>
+            <el-button type="primary" size="small" icon="View" title="添加SKU"></el-button>
+            <el-button type="primary" size="small" icon="Delete" title="添加SKU"></el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </el-card>
 
@@ -19,16 +26,44 @@
       :page-sizes="[3, 5, 7, 9]"
       :background="true"
       layout="prev, pager, next, jumper, ->, sizes, total"
-      :total="400"
+      :total="total"
+      @current-change="getHasSpu"
+      @size-change="changeSize"
     />
   </div>
 </template>
 
 <script lang='ts' setup>
-import { ref } from "vue";
+import type { HasSpuResponseData, Records } from "@/api/product/spu/type";
+import { ref, watch } from "vue";
+import useCategoryStore from "@/store/modules/category";
+import { reqHasSpu } from "@/api/product/spu";
+
+  const categoryStore = useCategoryStore()
+
+  const records = ref<Records>([])
   const scene = ref<number>(0)
   const pageNo = ref<number>(1)
   const pageSize = ref<number>(3)
+  const total = ref<number>(0)
+
+  watch(() => categoryStore.c3Id, () => {
+    if(!categoryStore.c3Id) return
+    getHasSpu()
+  })
+
+  const getHasSpu = async (pager = 1) => {
+    pageNo.value = pager
+    const result:HasSpuResponseData = await reqHasSpu(pageNo.value, pageSize.value, categoryStore.c3Id)
+    if(result.code == 200) {
+      records.value = result.data.records
+      total.value = result.data.total
+    }
+  }
+
+  const changeSize = () => {
+    getHasSpu()
+  }
 </script>
 
 <style lang='less' scoped>
