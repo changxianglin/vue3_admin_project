306c1d972460ca2d980b4239bc424694fbec4ee5
changxianglin <281532612@qq.com>
2025-02-18 19:46:10 +0800

    home userinfo

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="home">home</div>
+ <el-card>
+  <div class="box">
+    <img :src="userStore.avatar" class="avatar" />
+  <div>
+    <h3>{{getTime()}}好呀{{ userStore.username }}</h3>
+    <p>最好的运营平台</p>
+  </div>
+  </div>
+ </el-card>
 </template>
 
 <script lang='ts' setup>
+import useUserStore from '@/store/modules/user';
+import { getTime } from '@/utils/time';
 
+let userStore = useUserStore()
 </script>
 
-<style lang='less' scoped>
-
+<style scoped lang="scss">
+.box {
+  display: flex;
+  .avatar {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
+}
 </style>
