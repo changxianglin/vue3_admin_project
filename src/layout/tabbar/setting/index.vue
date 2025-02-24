12fca2c2899c7f6424cf1b3e2f4b11df53bc45f6
changxianglin <281532612@qq.com>
2025-02-24 20:44:39 +0800

    Update index.vue
    setting dark model

--- src/layout/tabbar/setting/index.vue
+++ src/layout/tabbar/setting/index.vue
@@ -1,7 +1,26 @@
 <template>
       <el-button size="small" icon="Refresh" circle @click="updateRefsh"></el-button>
       <el-button size="small" icon="FullScreen" circle @click="fullScreen"></el-button>
-      <el-button size="small" icon="Setting" circle></el-button>
+      <el-popover
+        placement="bottom"
+        title="主题设置"
+        :width="300"
+        trigger="hover"
+      >
+        <el-form>
+          <el-form-item label="主题颜色">
+            <el-color-picker size="small" v-model="color" show-alpha :predefine="predefineColors" />
+          </el-form-item>
+          <el-form-item size="small" label="暗黑模式">
+            <el-switch v-model="dark"     class="mt-2"
+    style="margin-left: 24px"
+    inline-prompt active-icon="MoonNight" inactive-icon="Sunny" />
+          </el-form-item>
+        </el-form>
+        <template #reference>
+          <el-button size="small" icon="Setting" circle></el-button>
+        </template>
+      </el-popover>
       <img :src="userStore.avatar" alt="" style="width: 24px; height: 24px; margin: 0 10px; border-radius: 50%;" />
       <!-- dropdown -->
       <el-dropdown>
@@ -20,7 +39,7 @@
 </template>
 
 <script lang='ts' setup>
-import { nextTick } from 'vue';
+import { nextTick, ref } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import userLayoutSettingStore from '@/store/modules/setting';
 import useUserStore from '@/store/modules/user';
@@ -55,6 +74,26 @@ const logout = async () => {
     })
   })
 }
+
+const dark = ref<boolean>(false)
+
+const color = ref('rgba(255, 69, 0, 0.68)')
+const predefineColors = ref([
+  '#ff4500',
+  '#ff8c00',
+  '#ffd700',
+  '#90ee90',
+  '#00ced1',
+  '#1e90ff',
+  '#c71585',
+  'rgba(255, 69, 0, 0.68)',
+  'rgb(255, 120, 0)',
+  'hsv(51, 100, 98)',
+  'hsva(120, 40, 94, 0.5)',
+  'hsl(181, 100%, 37%)',
+  'hsla(209, 100%, 56%, 0.73)',
+  '#c7158577',
+])
 </script>
 
 <script lang="ts">
