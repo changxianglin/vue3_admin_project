71f1ebf24b6dde90a2b554584a4dccb62a263a0d
changxianglin <281532612@qq.com>
2025-05-05 10:26:31 +0800

    add dataScreen main background

--- src/views/screen/components/tourist/index.vue
+++ src/views/screen/components/tourist/index.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>游客比例</div>
+  <div class=box>游客比例</div>
 </template>
 
 <script lang='ts' setup>
 
 </script>
 
-<style lang='less' scoped>
-
+<style lang='scss' scoped>
+  .box {
+    background: url(../../images/dataScreen-main-lb.png) no-repeat;
+    background-size: 100% 100%;
+  }
 </style>
