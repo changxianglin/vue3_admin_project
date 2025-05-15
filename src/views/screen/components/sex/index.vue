76b439d4d3d756b25c7db77b8c1ed9c698fb2e6e
changxianglin <281532612@qq.com>
2025-05-15 21:01:20 +0800

    male and female layout

--- src/views/screen/components/sex/index.vue
+++ src/views/screen/components/sex/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>性别比例</div>
+  <div class=box1>
+    <div class="title">
+      <p>男女比例</p>
+      <img src="../../images/dataScreen-title.png" alt="" srcset="">
+    </div>
+  </div>
 </template>
 
 <script lang='ts' setup>
 
 </script>
 
-<style lang='less' scoped>
-
+<style lang='scss' scoped>
+  .box1 {
+    width: 100%;
+    height: 100%;
+    background: url(../../images/dataScreen-main-cb.png) no-repeat;
+    background-size: 100% 100%;
+    margin: 20px 0;
+    .title {
+      margin-left: 20px;
+      p {
+        color: #fff;
+        font-size: 20px;
+      }
+    }
+  }
 </style>
