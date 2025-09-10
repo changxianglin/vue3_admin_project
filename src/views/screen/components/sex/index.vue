4924a3703fdd4f8621331b6d9bf0e76a3de9135d
changxianglin <281532612@qq.com>
2025-09-10 21:03:59 +0800

    new add famele and male charts

--- src/views/screen/components/sex/index.vue
+++ src/views/screen/components/sex/index.vue
@@ -11,53 +11,125 @@
       <div class="woman">
         <img src="../../images/woman.png" alt="" srcset="">
       </div>
-      <div class="rate">
-        <p>男士58%</p>
-        <p>女士42%</p>
-      </div>
+    </div>
+    <div class="rate">
+      <p>男士58%</p>
+      <p>女士42%</p>
+    </div>
+    <div class="charts" ref="charts">
+
     </div>
   </div>
 </template>
 
 <script lang='ts' setup>
+import { Right } from '@element-plus/icons-vue'
+import * as echarts from 'echarts'
+import { ref, onMounted } from 'vue'
 
+const charts = ref()
+
+onMounted(() => {
+  const myCharts = echarts.init(charts.value)
+  myCharts.setOption({
+    title: {
+      text: '男女比例',
+      textStyle: {
+        color: 'skyblue'
+      },
+      left: '40%',
+    },
+    xAxis: {
+      show: false,
+      min: 0,
+      max: 100
+    },
+    yAxis: {
+      type: 'category',
+      show: false,
+    },
+    series: [
+      {
+        type: 'bar',
+        data: [58],
+        barWidth: 20,
+        z: 100,
+        itemStyle: {
+          color: 'skyblue',
+          borderRadius: 20,
+        }
+      },
+      {
+        type: 'bar',
+        data: [100],
+        barWidth: 20,
+        barGap: '-100%',
+        itemStyle: {
+          color: 'pink',
+          borderRadius: 20,
+        }
+      }
+    ],
+    grid: {
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+    }
+  })
+})
 </script>
 
 <style lang='scss' scoped>
-  .box1 {
-    width: 100%;
-    height: 100%;
-    background: url(../../images/dataScreen-main-cb.png) no-repeat;
-    background-size: 100% 100%;
-    margin: 20px 0;
-    .title {
-      margin-left: 20px;
-      p {
-        color: #fff;
-        font-size: 20px;
-      }
-    }
-    .sex {
-      display: flex;
-      justify-content: center;
-      .man {
-        margin: 20px;
-        width: 111px;
-        height: 115px;
-        background: url('../../images/man-bg.png') no-repeat;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      .woman {
-        margin: 20px;
-        width: 111px;
-        height: 115px;
-        background: url('../../images/woman-bg.png') no-repeat;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
+.box1 {
+  width: 100%;
+  height: 100%;
+  background: url(../../images/dataScreen-main-cb.png) no-repeat;
+  background-size: 100% 100%;
+  margin: 20px 0;
+
+  .title {
+    margin-left: 20px;
+
+    p {
+      color: #fff;
+      font-size: 20px;
     }
   }
+
+  .sex {
+    display: flex;
+    justify-content: center;
+
+    .man {
+      margin: 20px;
+      width: 111px;
+      height: 115px;
+      background: url('../../images/man-bg.png') no-repeat;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .woman {
+      margin: 20px;
+      width: 111px;
+      height: 115px;
+      background: url('../../images/woman-bg.png') no-repeat;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+
+  .rate {
+    display: flex;
+    justify-content: space-between;
+    color: #fff;
+  }
+
+  .charts {
+    height: 100px;
+  }
+}
 </style>
