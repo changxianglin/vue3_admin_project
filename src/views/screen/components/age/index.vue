83d93798d6202ce58f22cbf63c0e43e3c673f97a
changxianglin <281532612@qq.com>
2025-09-11 21:03:41 +0800

    finish age charts

--- src/views/screen/components/age/index.vue
+++ src/views/screen/components/age/index.vue
@@ -1,11 +1,93 @@
 <template>
-  <div>年龄比例</div>
+  <div class="box2">
+    <div class="title">
+      <p>年龄比例</p>
+      <img src="../../images/dataScreen-title.png" alt="" srcset="">
+    </div>
+    <div class="charts" ref="charts">
+
+    </div>
+  </div>
 </template>
 
 <script lang='ts' setup>
+import { ref, onMounted } from 'vue'
+import * as echarts from 'echarts'
 
+const charts = ref()
+
+onMounted(() => {
+  let mycharts = echarts.init(charts.value)
+  let option = {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      top: 40,
+      right: 30,
+      orient: 'vertical',
+      textStyle: {
+        color: 'white',
+        fontSize: 14,
+      }
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2
+        },
+        label: {
+          show: true,
+          position: 'inside',
+          color: 'white',
+        },
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 1048, name: '军事' },
+          { value: 735, name: '新闻' },
+          { value: 580, name: '直播' },
+          { value: 484, name: '娱乐' },
+          { value: 300, name: '财经' }
+        ]
+      }
+    ],
+    grid: {
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+    }
+  };
+  mycharts.setOption(option)
+})
 </script>
 
-<style lang='less' scoped>
+<style lang='scss' scoped>
+.box2 {
+  width: 100%;
+  height: 100%;
+  background: url(../../images/dataScreen-main-cb.png) no-repeat;
+  background-size: 100% 100%;
 
+  .title {
+    margin-left: 20px;
+
+    p {
+      color: #fff;
+      font-size: 20px;
+    }
+  }
+
+  .charts {
+    height: 260px;
+  }
+}
 </style>
